1d61f834d01fe2042d81671e6c51c8331864879e
evgeniaalejnikova <evgeniaalejnikova@MacBook-Air-Evgenia.local>
2021-11-04 18:36:29 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,78 @@
+package ru.netology.domain;
+
+
+    public class Radio {
+        private int currentStation;
+        private int currentVolume;
+
+
+        public void setCurrentStation(int CurrentStation) {
+            if (CurrentStation < 0) {
+                return;
+            }
+            if (CurrentStation > 9) {
+                return;
+            }
+            this.currentStation = CurrentStation;
+        }
+
+        public void switchToNext() {
+            //  if (currentStation > 9) {
+            //     currentStation = 0;
+            //  }
+            if (currentStation < 9) {
+                currentStation = currentStation + 1;
+            }
+            else {
+                currentStation = 0;
+            }
+        }
+
+        public void switchToPrev() {
+            if (currentStation < 0) {
+                currentStation = 9;
+            }
+            if (currentStation > 0) {
+                currentStation = currentStation - 1;
+            }
+        }
+
+        public void setStationNumber() {
+            currentStation = 4;
+
+        }
+
+        public void setCurrentVolume(int currentVolume) {
+            if (currentVolume < 0) {
+                return;
+            }
+            if (currentVolume > 10) {
+                return;
+            }
+            this.currentVolume = currentVolume;
+        }
+
+        public void increaseVolume() {
+            if (currentVolume < 10) {
+                currentVolume = currentVolume + 1;
+            }
+        }
+
+        public void decreaseVolume() {
+            if (currentVolume < 0) {
+                currentVolume = currentVolume - 1;
+            }
+        }
+
+
+        public int getCurrentStation() {
+            return currentStation;
+
+        }
+
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+    }
+
+
